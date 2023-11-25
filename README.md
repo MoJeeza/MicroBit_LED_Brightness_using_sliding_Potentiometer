83a459f4dc0c1fafc21de7a70cbeaf79ab457b81
Mohsin Jessa <7672012+MoJeeza@users.noreply.github.com>
2023-11-25 03:06:14 -0500

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://mojeeza.github.io/microbit_led_brightness_using_sliding_potentiometer/](https://mojeeza.github.io/microbit_led_brightness_using_sliding_potentiometer/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/mojeeza/microbit_led_brightness_using_sliding_potentiometer** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/mojeeza/microbit_led_brightness_using_sliding_potentiometer** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,46 @@
+/**
+ * Author: Mohsin Jessa
+ * 
+ * Date: 24-Nov-2023
+ * 
+ * STEAMetics Canada
+ * 
+ * Items needed:
+ * 
+ * MicroBit
+ * 
+ * ElecFreaks-SensorBit
+ * 
+ * ElecFreaks-Sliding potentiometer
+ * 
+ * jumper cables (F-F)and(F-M for breadboard)
+ * 
+ * LED (any color but with good brightness level - RED is better)
+ * 
+ * Breadboard
+ * 
+ * Resistor
+ * 
+ * We use sliding Potentiometer connected to MicroBit's Pin 1 and an LED connected to the Signal and Ground of Pin2
+ * 
+ * The potentiometer's LED is connected to Pin8 (signal only)
+ * 
+ * The LED is connected on Breadboard while the MicroBit is connected to ElecFreak's SensorBit and powered by USB connection from Laptop.
+ * 
+ * Operation:
+ * 
+ * The external LED connected on the breadboard will change it's brightness based on the position of the slider of the potentiometer.
+ */
+let Off = 0
+let On = 1
+let P_val = 0
+basic.forever(function () {
+    P_val = pins.analogReadPin(AnalogPin.P1)
+    basic.showNumber(P_val)
+    pins.analogWritePin(AnalogPin.P2, P_val)
+    if (P_val > 500) {
+        pins.digitalWritePin(DigitalPin.P8, On)
+    } else {
+        pins.digitalWritePin(DigitalPin.P8, Off)
+    }
+})
