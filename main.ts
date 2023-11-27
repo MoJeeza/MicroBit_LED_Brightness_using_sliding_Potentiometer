03bce43ed4236738b351c050f1dc6deac081f2a0
Mohsin Jessa <7672012+MoJeeza@users.noreply.github.com>
2023-11-27 01:44:54 -0500

    This version plots graph on MicroBit LED and serial output

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /**
  * Author: Mohsin Jessa
  * 
- * Date: 24-Nov-2023
+ * Date: 27-Nov-2023
  * 
  * STEAMetics Canada
  * 
@@ -30,13 +30,19 @@
  * Operation:
  * 
  * The external LED connected on the breadboard will change it's brightness based on the position of the slider of the potentiometer.
+ * 
+ * This version will also plot a bar graph as the potentiometer slide switch is changing as well as show numerical values and plot a graph seen on the Serial Device
  */
 let Off = 0
 let On = 1
 let P_val = 0
 basic.forever(function () {
     P_val = pins.analogReadPin(AnalogPin.P1)
-    basic.showNumber(P_val)
+    led.plotBarGraph(
+    P_val,
+    1023,
+    true
+    )
     pins.analogWritePin(AnalogPin.P2, P_val)
     if (P_val > 500) {
         pins.digitalWritePin(DigitalPin.P8, On)
